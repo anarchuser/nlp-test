4e41feea70d5a67862a67529c8589eee82db440c
Aaron Alef <aaron.alef@code.berlin>
2019-02-19 16:36:09 +0100

    set up gRPC server and grpc proto files

--- server.py
+++ server.py
@@ -9,20 +9,27 @@ Server to transcript audio streams and send them back
 
 import socket
 import os
+import time
+import grpc
+from concurrent import futures
 
-from audioStream_pb2 import *
+import audioStream_pb2_grpc
 
 HOST = '127.0.0.1'  # Server name
 PORT = 12345         # Standard RTP port
 CHUNK = 128
+FOREVER = 10000000
 
-socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-socket.bind((HOST, PORT))
-socket.listen(5)
+def serve(UPTIME):
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    audioStream_pb2_grpc.add_AudioProcessorServicer_to_server(audioStream_pb2_grpc.AudioProcessorServicer, server)
+    server.add_insecure_port('[::]:' + str(PORT))
+    server.start()
+    try:
+        time.sleep(UPTIME)
+    except KeyboardInterrupt:
+        server.stop()
 
-while True:
-    connection, address = socket.accept()
-    data = connection.recv(CHUNK)
-
-    print(data)
-    os.system(str(data) + " >> test.wav")
+print("Start serving.")
+serve(FOREVER)
+print("Stop serving.")
