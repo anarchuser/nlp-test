92d1ed85ccf3027b65a8d41dc44cc34c7abc8179
Aaron Alef <aaron.alef@code.berlin>
2019-02-19 14:09:28 +0100

    Added compiled Proto file (also changed chunk size back)

--- server.py
+++ server.py
@@ -10,9 +10,11 @@ Server to transcript audio streams and send them back
 import socket
 import os
 
+from audioStream_pb2 import *
+
 HOST = '127.0.0.1'  # Server name
 PORT = 12345         # Standard RTP port
-CHUNK = 102400000
+CHUNK = 128
 
 socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 socket.bind((HOST, PORT))
