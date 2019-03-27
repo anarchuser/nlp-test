30b0e5b2a5cc4240e4992758dacdb0c7525696c8
Aaron Alef <aaron.alef@code.berlin>
2019-03-27 11:29:59 +0100

    Open mic stream and display it

--- microphone.py
+++ microphone.py
@@ -6,3 +6,19 @@ NOTEPADAI
 
 Sends microphone input to the processor, for testing purposes
 """
+
+import pyaudio
+import time
+
+CHUNK = 300
+
+p = pyaudio.PyAudio()
+stream = p.open(format=pyaudio.paInt8, channels=1, rate=16000, input=True, frames_per_buffer=CHUNK)
+
+try:
+    while True:
+        data = stream.read(CHUNK)
+        print(data)
+        time.sleep(10)
+except KeyboardInterrupt:
+    print("Stop recording")
