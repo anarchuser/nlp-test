f509f8cfbb2cc47d8875c6268909d7412e3841c2
fbachus <felix.bachstein@gmail.com>
2019-04-23 16:45:53 +0200

    integrated googles main class into Processor

--- transcription/processor.py
+++ transcription/processor.py
@@ -7,10 +7,23 @@ NOTEPADAI
 Provides tools to transcript an audio stream
 """
 
+from __future__ import division
+from google.cloud import speech
+from google.cloud.speech import enums
+from google.cloud.speech import types
+from six.moves import queue
+
+import pyaudio
 import librosa
 import numpy as np
 import time
+import re
+import sys
 
+
+# Audio recording parameters
+RATE = 16000
+CHUNK = int(RATE / 10)  # 100ms
 
 class Processor:
     def __init__(self):
@@ -20,9 +33,19 @@ class Processor:
         print("Start processing")
         self.isRunning = True
 
-        dummy_text = "Transmission!"
+        language_code = 'de-DE'  # a BCP-47 language tag
 
-        sample = [0]
-        while len(sample) > 0:
-            sample = stream.__next__()
-            yield dummy_text
+        client = speech.SpeechClient()
+        config = types.cloud_speech_pb2.RecognitionConfig(
+            encoding=enums.RecognitionConfig.AudioEncoding.LINEAR16,
+            sample_rate_hertz=RATE,
+            language_code=language_code)
+        streaming_config = types.cloud_speech_pb2.StreamingRecognitionConfig(
+            config=config,
+            interim_results=True)
+
+        requests = (types.cloud_speech_pb2.StreamingRecognizeRequest(audio_content=content)
+                    for content in stream)
+
+
+        return(client.streaming_recognize(streaming_config, requests))
