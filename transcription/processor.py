78d49c72d7f170f4560f6bd7cb574731a7a6fe40
Aaron Alef <aaron.alef@code.berlin>
2019-04-24 17:04:42 +0200

    Finished gcloud speech transcription

--- transcription/processor.py
+++ transcription/processor.py
@@ -33,14 +33,14 @@ print(os.environ["GOOGLE_APPLICATION_CREDENTIALS"])
 
 
 class Processor:
-    def __init__(self):
-        self.isRunning = False
+    def __init__(self, lang="de_DE"):
+        self.lang = lang
 
     def process(self, stream):
         print("Start processing")
         self.isRunning = True
-
-        language_code = 'de-DE'  # a BCP-47 language tag
+        
+        language_code = self.lang
 
         client = speech.SpeechClient()
         config = types.cloud_speech_pb2.RecognitionConfig(
@@ -54,4 +54,16 @@ class Processor:
         requests = (types.cloud_speech_pb2.StreamingRecognizeRequest(audio_content=content)
                     for content in stream)
 
-        return client.streaming_recognize(streaming_config, requests)
+        for response in client.streaming_recognize(streaming_config, requests):
+            if not response.results:
+                continue
+
+            result = response.results[0]
+            if not result.alternatives:
+                continue
+
+            if not result.is_final:
+                continue
+
+            yield result.alternatives[0].transcript
+
