3c107ff82b2b1f3657245cc69014314a63b1986b
Aaron Alef <aaron.alef@code.berlin>
2019-04-26 13:56:52 +0200

    Added optional params for starting the server

--- transcription/server.py
+++ transcription/server.py
@@ -17,19 +17,21 @@ HOST = '192.168.44.103'     # Server name
 PORT = 12345                # Server port
 FOREVER = 1000000           # Large number to keep the server running.
 WORKERS = 8                 # Max. amount of simultaneous threads
+
+DEFAULT_LANG = "de_DE"      # The default target language
 """"""
 
 
 # Class handling the AudioStreamServicer internally
 class Server:
-    def __init__(self, host=HOST, port=PORT, uptime=FOREVER, workers=WORKERS):
+    def __init__(self, host=HOST, port=PORT, uptime=FOREVER, workers=WORKERS, argv=None):
         # Set up and start the server
         print("Route servant - Default IP: " + host)
         ip = self.get_valid_address(default=host)
 
         print("Conceive servant")
         print(ip)
-        self.servant = AudioProcessorServicer(host=ip, port=port, uptime=uptime, workers=workers)
+        self.servant = AudioProcessorServicer(host=ip, port=port, uptime=uptime, workers=workers, argv=argv)
 
     def start(self):
         print("Enliven servant")
@@ -65,18 +67,26 @@ def string_to_response(word):
 # Actual server connecting to the outer world.
 # Only used by the Server class
 class AudioProcessorServicer(audioStream_pb2_grpc.AudioProcessorServicer):
-    def __init__(self, host, port, uptime, workers):
+    def __init__(self, host, port, uptime, workers, argv):
         print("Mark servant (" + str(port) + ")")
         self.host = host
         self.port = port
         self.uptime = uptime
         self.workers = workers
 
+        # Parse processor parameters from the argv parameter list
+        self.lang = DEFAULT_LANG if len(argv) < 1 else argv[0]
+        self.print = False if len(argv) < 2 else bool(argv[1])
+        self.send_interim_results = False if len(argv) < 3 else bool(argv[2])
+
         self.server = grpc.server(futures.ThreadPoolExecutor(max_workers=self.workers))
 
     def transcriptAudio(self, request_iterator, context):
         print("Connection received")
-        for word in Processor().process(request_iterator):
+        processor = Processor(lang=self.lang, send_interim_results=self.send_interim_results)
+        for word in processor.process(request_iterator):
+            if self.print:
+                print(word)
             yield string_to_response(word)
         print("Connection lost")
 
