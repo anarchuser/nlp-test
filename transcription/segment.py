a836fb2a8636558b24113ec3c438479e661044f3
Aaron Alef <aaron.alef@code.berlin>
2019-05-02 15:08:32 +0200

    Added skeleton for the segment class

---
+++ transcription/segment.py
@@ -0,0 +1,29 @@
+"""
+NOTEPADAI
+(Prepare)
+
+A tool to segment the audio samples (spoken and written words) into phonemes
+"""
+
+FORMAT = "tsv"
+FILES = [
+    "dev",
+    "invalidated",
+    "other",
+    "test",
+    "train",
+    "validated"
+]
+
+class Segment:
+    def __init__(self, path='./'):
+        self.path = path
+
+    def __load_tables(self):
+        pass
+
+    def __segment_text:
+        pass
+
+    def __segment_speech:
+        pass
