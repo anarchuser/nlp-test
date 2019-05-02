b6964afa6e54c979d76374e949523ed3caed01b4
Aaron Alef <aaron.alef@code.berlin>
2019-05-02 16:04:09 +0200

    implemented load_tables method & added TODOs for the segmentation methods

--- transcription/segment.py
+++ transcription/segment.py
@@ -5,8 +5,12 @@ NOTEPADAI
 A tool to segment the audio samples (spoken and written words) into phonemes
 """
 
+import pandas as pd
+
+import os
+
 FORMAT = ".tsv"
-FILES = [
+TABLES = [
     "dev",
     "invalidated",
     "other",
@@ -19,12 +23,24 @@ FILES = [
 class Segment:
     def __init__(self, path='./'):
         self.path = path
+        self.tables = {}
 
+    # Load all tsv files into a dict
     def __load_tables(self):
-        pass
+        for table in TABLES:
+            self.tables.update([table, pd.read_csv(os.path.join(self.path, table + FORMAT), sep='\t')])
 
     def __segment_text(self):
+        # TODO:
+        #  Read sentence
+        #  Segment it
+        #  Add it to table
         pass
 
     def __segment_speech(self):
+        # TODO:
+        #  Load mp3 file
+        #  Convert it to wav
+        #  Look how many phonemes there should be
+        #  Try to locate them (-> TRAIN A CLASSIFIER!!!)
         pass
