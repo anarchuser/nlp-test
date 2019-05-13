dc8421c3d5de9e6cd50ec5a9e62452de4879c031
Aaron Alef <aaron.alef@code.berlin>
2019-05-13 14:57:41 +0200

    Added visualiser

--- transcription/visualise.py
+++ transcription/visualise.py
@@ -1,31 +1,3 @@
-<<<<<<< HEAD
-"""
-NOTEPADAI
-(Visualiser)
-
-A data science tool to produce cool graphs accompanying the process of speech recognition
-"""
-
-
-import librosa
-
-CHUNK = 320
-
-
-class Visualise:
-    def __init__(self, file_path):
-        self.file = file_path
-
-    def show_mfcc(self):
-        pass
-
-    def as_mfcc_stream(self):
-
-
-    def as_stream(self):
-        pass
-||||||| merged common ancestors
-=======
 """
 NOTEPADAI
 (Visualiser)
@@ -37,4 +9,3 @@ A data science tool to produce cool graphs accompanying the process of speech re
 class Visualise:
     def __init__(self):
         pass
->>>>>>> Added visualiser
