1e23af63c390a9e844d78a7be57a981a2a46e1cb
Aaron Alef <aaron.alef@code.berlin>
2019-05-16 18:05:43 +0200

    Improved jupyter notebook [NLP]

--- transcription/visualise.py
+++ transcription/visualise.py
@@ -9,6 +9,8 @@ A data science tool to produce cool graphs accompanying the process of speech re
 from transcription.helper import *
 
 import librosa
+import vlc
+import matplotlib.pyplot as plt
 
 CHUNK = 320
 
@@ -16,21 +18,39 @@ CHUNK = 320
 class Visualise:
     def __init__(self, path):
         self.path = path
-        self.file = librosa.load(path)
+        self.data, self.sample_rate = librosa.load(path)
+        self.__player__ = vlc.MediaPlayer(path)
 
     def play(self):
-        pass
+        self.__player__.play()
 
-    def show_mfcc(self):
-        pass
+    def mfcc(self):
+        return [convert(data) for data in self.as_mfcc_stream()]
 
-    def show_mfcc_d(self):
+    def mfcc_d(self):
         pass
 
     def as_mfcc_stream(self, chunk=CHUNK):
-        for window in audio_to_stream(self.file, chunk):
+        for window in audio_to_stream(self.data, chunk):
             yield librosa.feature.mfcc(window)
 
-    def as_stream(self, chunk=CHUNK):
-        return audio_to_stream(self.file, chunk)
+    def as_audio_stream(self, chunk=CHUNK):
+        return audio_to_stream(self.data, chunk)
 
+
+def specgram(obj, xlabel=None, ylabel=None, axis=None, chunk=CHUNK):
+    s = plt
+    s.specgram(obj.data, NFFT=chunk, Fs=obj.sample_rate)
+
+    if xlabel is not None:
+        s.xlabel(xlabel)
+    if ylabel is not None:
+        s.ylabel(ylabel)
+    if axis is not None:
+        s.axis(axis)
+
+    s.show()
+
+
+def convert(coefficients):
+    return [data for data in [pair for pair in coefficients]]
