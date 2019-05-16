ab74a3ec5ebb7ac15b3a91a22b33e28504368d51
Aaron Alef <aaron.alef@code.berlin>
2019-05-16 15:46:29 +0200

    Even more bones. Also, some flesh to create streams

--- transcription/visualise.py
+++ transcription/visualise.py
@@ -6,6 +6,31 @@ A data science tool to produce cool graphs accompanying the process of speech re
 """
 
 
+from transcription.helper import *
+
+import librosa
+
+CHUNK = 320
+
+
 class Visualise:
-    def __init__(self):
+    def __init__(self, path):
+        self.path = path
+        self.file = librosa.load(path)
+
+    def play(self):
         pass
+
+    def show_mfcc(self):
+        pass
+
+    def show_mfcc_d(self):
+        pass
+
+    def as_mfcc_stream(self, chunk=CHUNK):
+        for window in audio_to_stream(self.file, chunk):
+            yield librosa.feature.mfcc(window)
+
+    def as_stream(self, chunk=CHUNK):
+        return audio_to_stream(self.file, chunk)
+
